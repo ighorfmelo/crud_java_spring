9941c35922b1c6d9b284637a1f1d93db80cd570e
Ighor Melo <ighor.melo@aluno.ufabc.edu.br>
2024-03-03 19:15:48 -0300

    adicionar endpoint de edição

--- src/main/java/com/example/crud/domain/product/RequestProduct.java
+++ src/main/java/com/example/crud/domain/product/RequestProduct.java
@@ -1,5 +1,10 @@
 package com.example.crud.domain.product;
 
-public record RequestProduct(String name, Integer price_in_cents) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Optional;
+
+public record RequestProduct(String id, @NotBlank String name, @NotNull Integer price_in_cents) {
 
 }
